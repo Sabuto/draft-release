c4345e0f0feadd6e498343aae17b79a24595d860
Sabuto <robe_dunne@hotmail.com>
2021-03-20 23:01:14 +0000

    use commits constant

--- dist/index.js
+++ dist/index.js
@@ -61,6 +61,7 @@ async function run() {
       });
       let body = draftObj.body;
       commits.forEach(obj => {
+        core.info(JSON.stringify(obj, null, 3));
         body += `* ${obj.message} @${obj.comitter.username}`
       })
       core.info(body);
