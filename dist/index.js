b463073ee4fd46cef5c5b84f0edafd97fca9836c
Sabuto <robe_dunne@hotmail.com>
2021-03-20 20:45:22 +0000

    create draft if none exist

--- dist/index.js
+++ dist/index.js
@@ -55,7 +55,7 @@ async function run() {
       core.info("No Draft found.... Creating new draft.");
       core.info(`latest tag: ${lastRelease}`);
       core.info(`Creating draft release with the tag ${major}.${minor}.${patch}`);
-      core.info(payload.message);
+      core.info(JSON.stringify(payload));
       await octokit.repos.createRelease({
         owner: owner,
         repo: repo,
