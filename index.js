fedda1c0efbee43862f008c679b386c7a45ee7ea
Sabuto <robe_dunne@hotmail.com>
2021-03-20 17:35:59 +0000

    retrieve the latest tag from the repo for version number

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function run() {
   try {
     // set the success output to false to begin with
     core.setOutput('success', false);
+    const prefix = core.getInput('prefix');
 
     // create a new instance so we can interact with the github api
     const octokit = github.getOctokit(core.getInput('token'));
@@ -17,22 +18,30 @@ async function run() {
     const owner = splitRepo[0];
     const repo = splitRepo[1];
 
-    const {data: issues} = await octokit.repos.listReleases({
+    const {data: releases} = await octokit.repos.listReleases({
       owner: owner,
       repo: repo
     });
 
-    let draft = issues.filter(obj => {
+    let draft = releases.filter(obj => {
       if(obj.draft == true && obj.name.startsWith("RL Drafter:")){
         return true;
       }
     });
 
+    const {tag_name: tag} = Object.values(releases)[0];
+
     if (Object.keys(draft).length !== 0) {
       core.info(Object.keys(draft).length);
       core.info(JSON.stringify(draft));
     } else {
-      core.info("No Draft found.... implement creating.");
+      core.info("No Draft found.... Creating new draft.");
+      core.info(`latest tag: ${tag}`);
+      // await octokit.repos.createRelease({
+      //   owner: owner,
+      //   repo: repo,
+      //   tag_name: `${prefix}`
+      // })
     }
 
     // Get the prefix from the inputs
