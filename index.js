3e4822d52476aedd11a80a809893fa1b44678853
Sabuto <robe_dunne@hotmail.com>
2021-03-20 11:42:54 +0000

    updated token

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function run() {
 
     // Get the prefix from the inputs
     //const prefix = core.getInput('prefix', {required: true});
-    core.info(octokit);
+    core.info(JSON.stringify(octokit));
   } catch (error) {
     core.setFailed(error.message);
   }
